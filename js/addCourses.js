b428675a5acd8f473838802ae9b5c7b5b0ed56c6
UnintelligentFool <89090904+UnintelligentFool@users.noreply.github.com>
2022-02-27 04:37:25 +0100

    Death approaches!

--- js/addCourses.js
+++ js/addCourses.js
@@ -1,4 +1,4 @@
-let coursesArray = [];
+//let coursesArray = [];
 
 const addCourseToArray = (stopButtonRefreshing) => {
 
@@ -15,20 +15,44 @@ const addCourseToArray = (stopButtonRefreshing) => {
 
     }
 
-    coursesArray.push(course);
-    console.log(`Added course to array: ${kurstitel.value}`);
+    //coursesArray.push(course);
+    //console.log(`Added course to array: ${kurstitel.value}`);
     
-    for (let i = 0; i < coursesArray.length; i++) {
+    //for (let i = 0; i < coursesArray.length; i++) {
 
-        const newCourse = coursesArray[i];
+        //const newCourse = coursesArray[i];
         
-        let addCourseToList = `<li>${newCourse.id}</li>`
+        //let addCourseToList = `<li class="addedCoursesHideDecorations">
+        let addCourseToList = `<div>
+        
+        
+        
+        
+        
+        <a href="#">
+    <div class="row container coursesCell">
+        <img src="${kursbild.value}" class="col-4 float-start coursesImages" alt="${kurstitel.value}" />
+        <div class="col-8 float-end coursesTextSubcell">
+            <h5>${kurstitel.value}</h5>
+            <p>${kursbeskrivning.value}</p>
+            <br />
+            <p><b><i>Kursnummer: </i></b>${kursnummer.value}</p>
+            <p><b><i>Kurslängd: </i></b>${kurslangd.value}</p>
+        </div>
+    </div>
+</a>
+        
+        
+        
+        
+        
+        
+        </div>`//</li>`
 
-        document.getElementById("deathApproaches").innerHTML += addCourseToList;
+        //document.getElementById("deathApproaches").innerHTML += addCourseToList;
+        document.getElementById("soonTimeIsUp").innerHTML += addCourseToList;
 
-
-
-    }
+    //}
 
     document.querySelector("form").reset();
 
